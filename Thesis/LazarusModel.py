fix: tangent prediction and curve sign

Thesis/LazarusModel.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from .StepSizeMyAcceptance import StepSizeMyAcceptance
 from .PredictorMyTangent import PredictorMyTangent
+from .SolverMyNewtonRaphson import SolverMyNewtonRaphson
 
 # Build the linear part of the Duffing oscillator
 linsys = dict()
@@ -13,7 +14,7 @@ linsys["K"] = np.array([1])
 linsys["G"] = 0 * linsys["M"]
 
 MODEL = {
-    "plugin": [StepSizeMyAcceptance, PredictorMyTangent],
+    "plugin": [StepSizeMyAcceptance, PredictorMyTangent, SolverMyNewtonRaphson],
     "analysis": {},
     "system": {},
     "substructures": {
@@ -54,26 +55,30 @@ CHUNCK_ALL = {
     "analysis": {
         "cont": {
             "study": "frf",
+
+            # # Quick test on first peak
+            # "puls_inf": 0.01,
+            # "puls_start": 0.1,
+            # "puls_sup": 0.3,
+            # "ds0": 1e-2,
+            # "ds_min": 1e-8,
+            # "ds_max": 5e-2,
+
             "puls_inf": 0.01,
             "puls_start": 0.1,
-            # "puls_sup": 5.0,
-            "puls_sup": 0.3,
-
-            "ds0": 1e-2,
+            "puls_sup": 5.0,
+            "ds0": 5e-3,
             "ds_min": 1e-8,
             "ds_max": 5e-2,
 
-            # "ds0": 1e-1,
-            # "ds_min": 1e-3,
-            # "ds_max": 2e-1,
-
             "sign_ds": 1,
             "verbose": True,
-            "stepsizer": "myacceptance",
+            "stepsizer": "acceptance",
             "predictor": "tangent",
             "corrector": "arc_length",
             "stopper": "bounds",
-            "solver": "scipyroot",
+            "solver": "MyNewtonRaphson",
+            # "solver": "NewtonRaphson",
         },
     },
 }

Thesis/StepSizeHybrid.py
@@ -46,7 +46,7 @@ class StepSizeHybrid(ABCStepSizeRule):
         sol = sollist[-1]
 
         if (not sol.flag_accepted) and (ds > self.ds_min):
-            ds /= 6
+            ds /= 3
         try:
             acc = np.array([sol.flag_accepted for sol in sollist[-self.consecutive_accept : :]])
             if np.sum(acc) == self.consecutive_accept and ds < self.ds_max:

src/pyHarm/Predictors/ABCPredictor.py
@@ -54,6 +54,7 @@ class ABCPredictor(abc.ABC):
     default_options = {
         "norm": "norm1",
         "bifurcation_detect": True,
+        "blind_spot": 30,
         "verbose": True,
     }
     """
@@ -65,12 +66,14 @@ class ABCPredictor(abc.ABC):
     angular frequency.
     It contains a bifurcation detection using the 'bifurcation_detect' keyword that can be set to
     True (default) if detection is needed.
+    It contains a 'blind_angle' keyword that sets a blind angle to avoid the predictor to search in
+    the inwards direction.
     It contains a 'verbose' keyword that can be set to True (default) if information about detection
     of bifurcations is to be displayed during solving.
     """
 
-    def __init__(self, dir, **kwargs):
-        self.dir = dir
+    def __init__(self, sign_ds, **kwargs):
+        self.sign_ds = sign_ds
         self.predictor_options = getCustomOptionDictionary(kwargs, self.default_options)
         self.flag_print = self.predictor_options["verbose"]
 
@@ -90,7 +93,7 @@ class ABCPredictor(abc.ABC):
         """
         pass
 
-    def bifurcation_detect(self, lstpt: SystemSolution):
+    def bifurcation_detect(self, last_sol: SystemSolution):
         """
         Makes a bifurcation detection analysis computing determinant of jacobian matrix
         and analyzing change of sign.
@@ -103,28 +106,37 @@ class ABCPredictor(abc.ABC):
             sign_ds (float): Attribute is modified if a fold bifurcation is detected.
             bifurcation_type (BifurcationType): Attribute is assigned if a bifurcation is detected.
         """
-        Jaco = lstpt.get_jacobian("full")
+        J = last_sol.get_jacobian("full")
+        prev_sol = last_sol.precedent_solution
         # XXX: heavy to compute dets. Consider bordering techniques
-        det_J_f = spl.det(Jaco)
-        det_J_x = spl.det(Jaco[:-1, :-1])
-        lstpt.det_J_f = det_J_f
-        lstpt.det_J_x = det_J_x
-        if isinstance(lstpt, FirstSolution):
-            det_J_x_prec = det_J_x
-            det_J_f_prec = det_J_f
+        det_J = spl.det(J)
+        det_J_x = spl.det(J[:-1, :-1])
+        last_sol.det_J_f = det_J
+        last_sol.det_J_x = det_J_x
+        if isinstance(last_sol, FirstSolution):
+            det_J_x_prev = det_J_x
+            det_J_f_prev = det_J
         else:
-            det_J_x_prec = lstpt.precedent_solution.det_J_x
-            det_J_f_prec = lstpt.precedent_solution.det_J_f
+            det_J_x_prev = prev_sol.det_J_x
+            det_J_f_prev = prev_sol.det_J_f
 
-        if np.sign(det_J_x) != np.sign(det_J_x_prec):
-            lstpt.flag_bifurcation = True
-            if np.sign(det_J_f) == np.sign(det_J_f_prec) or np.sign(det_J_f) != np.sign(det_J_x):
-                lstpt.bifurcation_type = BifurcationType.FOLD
+        if np.sign(det_J_x) != np.sign(det_J_x_prev):
+            last_sol.flag_bifurcation = True
+            if np.sign(det_J) == np.sign(det_J_f_prev) or np.sign(det_J) != np.sign(det_J_x):
+                last_sol.bifurcation_type = BifurcationType.FOLD
             else:
-                lstpt.bifurcation_type = BifurcationType.BRANCHING
-
+                last_sol.bifurcation_type = BifurcationType.BRANCHING
+                self.sign_ds *= -1
             if self.flag_print:
-                print(f"Warning: a {lstpt.bifurcation_type.value} was detected")
+                print(f"Warning: a {last_sol.bifurcation_type.value} was detected")
+        elif (
+            not isinstance(last_sol, FirstSolution)
+            and np.dot(prev_sol.dir, last_sol.dir) < -np.cos(np.deg2rad(self.predictor_options["blind_spot"]/2))
+        ):
+            last_sol.flag_bifurcation = True
+            self.sign_ds *= -1
+            if self.flag_print:
+                print("A potential higher order bifurcation is detected")
 
     @staticmethod
     def get_last_point(sollist: list[SystemSolution], k_imposed=None) -> SystemSolution:

src/pyHarm/Predictors/PredictorTangent.py
@@ -1,8 +1,22 @@
+# Copyright 2024 SAFRAN SA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 from scipy import linalg
 
 from pyHarm.Predictors.ABCPredictor import ABCPredictor
-from pyHarm.Solver import FirstSolution, SystemSolution
+from pyHarm.Solver import SystemSolution
 
 
 class PredictorTangent(ABCPredictor):
@@ -28,32 +42,17 @@ class PredictorTangent(ABCPredictor):
             SystemSolution: last accepted point in the list of solutions.
         """
         last_sol = self.get_last_point(SolList, k_imposed)
-        prev_sol = last_sol.precedent_solution
-        last_sol.get_jacobian("full")  # this makes lstpt.J_f available
-        solx_len = last_sol.J_f[:-1, :-1].shape[0]
+
+        # See Allgower, p.29 : Jacobian tangent from its QR decomposition
+        last_sol.get_jacobian("full")  # this makes last_sol.J_f available
+        Q, R = linalg.qr(np.transpose(last_sol.J_f[:-1,:]))
+        sign = - np.sign(np.prod(np.diag(R)))  # sign = sign(det(R)*det(Q)), with det(Q) = 1
+        last_sol.dir = sign * Q[:, -1]
 
         if self.predictor_options["bifurcation_detect"]:
             self.bifurcation_detect(last_sol)
 
-        if isinstance(last_sol, FirstSolution):
-            prev_dir = np.vstack((np.zeros((solx_len, 1)), last_sol.sign_ds))
-        else:
-            prev_dir = prev_sol.dir
-        A = np.vstack((last_sol.J_f[:-1, :], prev_dir.T))
-        b = np.vstack((np.zeros((solx_len, 1)), 1))
-        dir = linalg.solve(A, b).ravel()
-        dir = self.normalize(dir)
-
         # Euler prediction: step along the tangent to the solution branch
-        xpred = last_sol.x + dir * ds
+        last_sol.x_pred = last_sol.x + self.sign_ds * ds * last_sol.dir
 
-        # # Debug prints
-        # print(f"{ds=}, {self.sign_ds=}")
-        # print(f"{dir=}")
-        # print(f"{xpred=}")
-
-        # write in the previous computed solution
-        last_sol.dir = dir
-        last_sol.x_pred = xpred
-
-        return xpred, last_sol
+        return last_sol